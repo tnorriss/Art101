7c171f187684002e5984461659efe6a8de19a424
tnorriss <tnorriss@ucsc.edu>
2024-05-06 16:41:59 -0700

    funcitoncreated

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,7 +1,22 @@
+//array
 numbers = [1,4,9,16,25];
-function squareNumber(numbers) { 
-    return numbers*numbers;
+
+//function
+function squareNumber(num) {
+    return num * num;
 }
 
-const squaredNumber = numbers.map(squareNumber) ; 
-console.log (squareNumbers);
+const squaredNumbers = numbers.map(squareNumber);
+
+console.log(squaredNumbers);
+
+ // Your map results data
+ const mapResults = squaredNumbers.join(", ");
+ 
+
+ 
+ // Use jQuery to select the element by its ID and set the HTML content
+ $("#output").html(mapResults);
+
+ console.log(mapResults);
+ 
